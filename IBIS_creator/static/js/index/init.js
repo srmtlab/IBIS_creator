3fa73e3fb3afbc770ef420fa0f3e6ccd8af22a41
akamiya208 <1235ak81@gmail.com>
2019-03-03 17:26:40 +0900

    テーマ作成時に，二回送信するバグを修正

--- IBIS_creator/static/js/index/init.js
+++ IBIS_creator/static/js/index/init.js
@@ -43,19 +43,7 @@ function create_theme() {
 
     document.createTheme.action = base_url + "/api/make_theme/";
     document.createTheme.submit();
-    $.ajax({
-        type: 'POST',
-        url: base_url + "/api/make_theme/",
-        data:
-            {
-                'name' : document.getElementById("create-theme-name").value,
-                'description' : document.getElementById("create-theme-description").value
-            },
-    }).fail(function(data){
-        // if data sending is failed
-        alert("テーマの作成に失敗しました");
-    });
-
+    
     $('#create_theme_modal').modal('hide');
 }
 
